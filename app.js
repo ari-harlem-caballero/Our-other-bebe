b53f6debdc8db34fb078af4244eef7bb2c3db31d
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-11-16 13:43:54 -0800

    added css color, created event listeners for quotes

--- app.js
+++ app.js
@@ -6,3 +6,21 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const moiraQuote = document.getElementById('quote');
+const foldButton = document.getElementById('fold-button');
+const cheeseVideo = document.getElementById('video-clip');
+let clickCounter = 0;
+
+foldButton.addEventListener('click', () => {
+  clickCounter++
+  
+  if (clickCounter === 1) {
+    moiraQuote.textContent = 'Just fold the cheese, David.'
+  } 
+  if (clickCounter === 2) {
+    moiraQuote.textContent = 'If you say fold the cheese one more time.'
+  }
+  if (clickCounter === 3) {
+    
+  }
+});
