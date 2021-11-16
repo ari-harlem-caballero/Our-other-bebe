3720f1aa8d340b63446b8ed5a2a378f53472f556
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-11-16 13:57:37 -0800

    added refoldButton event listener and 3rd click

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
 const moiraQuote = document.getElementById('quote');
 const foldButton = document.getElementById('fold-button');
 const cheeseVideo = document.getElementById('video-clip');
+const refoldButton = document.getElementById('refold-button');
 let clickCounter = 0;
 
 foldButton.addEventListener('click', () => {
@@ -21,6 +22,18 @@ foldButton.addEventListener('click', () => {
     moiraQuote.textContent = 'If you say fold the cheese one more time.'
   }
   if (clickCounter === 3) {
-    
+    cheeseVideo.style.display = 'block';
+    moiraQuote.style.display = 'none';
+    foldButton.style.display = 'none';
+    refoldButton.style.display = 'block';
   }
+});
+
+refoldButton.addEventListener('click', () => {
+  cheeseVideo.style.display = 'none';
+  moiraQuote.style.display = 'block';
+  moiraQuote.textContent = 'Next step is to fold the cheese.'
+  refoldButton.style.display = 'none';
+  foldButton.style.display = 'block';
+  clickCounter = 0;
 });
